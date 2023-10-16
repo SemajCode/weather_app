6985cabf5b6211ab5b409facec5366392e48a369
SemajCode <mfonjames23@gmail.com>
2023-10-16 21:50:27 +0100

    laying out the app structure

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -12,8 +12,236 @@ class _WeatherScreenState extends State<WeatherScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [],
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 12,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Top elements
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Nigeria',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text('17 October 2023',
+                          style: TextStyle(
+                            fontSize: 18,
+                          )),
+                    ],
+                  ),
+                  Icon(
+                    Icons.refresh,
+                    size: 34,
+                  ),
+                ],
+              ),
+
+              //Divider
+              Divider(),
+
+              SizedBox(height: 24),
+
+              //Main Weather information
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '32 °C',
+                        style: TextStyle(
+                          fontSize: 72,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'sunny',
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Icon(
+                    Icons.sunny,
+                    size: 98,
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+
+              //Additional Weather information
+              Card(
+                elevation: 4,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.cloud,
+                            size: 38,
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            '15 m/s',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Wind',
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.cloud,
+                            size: 38,
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            '15 m/s',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Wind',
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.cloud,
+                            size: 38,
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            '15 m/s',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Wind',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 24),
+
+              // buttom hourly forecast
+              Text(
+                'Today',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      elevation: 4,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text('9 AM'),
+                            SizedBox(height: 8),
+                            Icon(Icons.cloud),
+                            SizedBox(height: 8),
+                            Text(
+                              '16°',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      elevation: 4,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text('9 AM'),
+                            SizedBox(height: 8),
+                            Icon(Icons.cloud),
+                            SizedBox(height: 8),
+                            Text(
+                              '16°',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      elevation: 4,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text('9 AM'),
+                            SizedBox(height: 8),
+                            Icon(Icons.cloud),
+                            SizedBox(height: 8),
+                            Text(
+                              '16°',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
