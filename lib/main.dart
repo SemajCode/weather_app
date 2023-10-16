e3730510e6e077976254188bac8831722f87743c
SemajCode <mfonjames23@gmail.com>
2023-10-16 20:53:39 +0100

    Starting the weather app on my own

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/weather_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true),
+      title: 'Weather App',
       home: const WeatherScreen(),
-    );
-  }
+    ),
+  );
 }
